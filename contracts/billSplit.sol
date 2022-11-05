3b5b6edf2d20e687ecc723e25f6e2db92ddad7f2
Karan Patel <karan2605@hotmail.co.uk>
2022-11-05 11:21:55 +0000

    imported dependencies

--- contracts/billSplit.sol
+++ contracts/billSplit.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+import "./Token.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract billSplit {
     constructor() {
         
