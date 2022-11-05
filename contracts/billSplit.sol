5d31486e4da7289a333d5f53ae50a865cd2dc90a
Karan Patel <karan2605@hotmail.co.uk>
2022-11-05 11:23:54 +0000

    Allow the contract to receive ether

--- contracts/billSplit.sol
+++ contracts/billSplit.sol
@@ -30,6 +30,12 @@ contract billSplit {
         }
     }
 
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
     constructor() {
         
     }
