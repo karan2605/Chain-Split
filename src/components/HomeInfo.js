1699329df32fff437cde640989159e57c2af24f6
Karan Patel <karan2605@hotmail.co.uk>
2022-11-14 19:43:06 +0000

    Info cards to be placed on the home page

---
+++ src/components/HomeInfo.js
@@ -0,0 +1,58 @@
+import { Card } from "flowbite-react/lib/cjs/components/Card";
+
+const HomeInfo = () => {
+    return(
+        <div className="grid grid-cols-3 gap-4 pr-10 pl-10 justify-center h-2/4 px-20 py-20">
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+            <Card className="border-4 border-purple-600 bg-stone-900">
+                <h5 className="text-2xl font-bold tracking-tight text-white">
+                Feature
+                </h5>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </Card>
+        </div>
+    )
+}
+
+export default HomeInfo;
