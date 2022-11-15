91e0fa586750252ceaa31cbfad3c58de077535e6
Karan Patel <karan2605@hotmail.co.uk>
2022-11-15 17:28:11 +0000

    Added dashboard elements and changed layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,23 @@
 import DashboardSidebar from "./DashboardSidebar"; 
 import DashboardBar from "./DashboardBar";
 import SiteFooter from "./SiteFooter";
+import RecentActivity from "./dashboardComponents/RecentActivity";
+import Friends from "./dashboardComponents/Friends";
+import PendingSplits from "./dashboardComponents/PendingSplits";
+import Stats from "./dashboardComponents/Stats";
 
 const Dashboard = () => {
     return (
         <div className="flex flex-col">
             <DashboardBar />
-            <div className="grid grid-rows-3 grid-cols-4 h-screen bg-stone-900">
+            <div className="flex flex-row">
                 <DashboardSidebar />
-                
+                <div className="grid grid-rows-2 grid-cols-3 h-screen bg-stone-900 gap-6 px-20 py-20 w-full">
+                    <RecentActivity />
+                    <Friends />
+                    <PendingSplits />
+                    <Stats />
+                </div>
             </div>
             <SiteFooter/>
         </div>
