efc10a84e7ac705628a0531f0f5c323ab34e63a5
Karan Patel <karan2605@hotmail.co.uk>
2022-11-15 17:29:26 +0000

    Added card content

--- src/components/dashboardComponents/RecentActivity.js
+++ src/components/dashboardComponents/RecentActivity.js
@@ -1,6 +1,26 @@
+import { Card } from "flowbite-react/lib/cjs/components/Card/Card";
 
 const RecentActivity = () => {
-
+    return (
+        <Card className={"dark:border-4 dark:border-purple-600 dark:bg-stone-900"}>
+            <div className="flex flex-col">
+                <div className="mb-4 flex justify-between">
+                    <h1 className="text-3xl font-bold leading-none text-gray-900 dark:text-white">
+                        Recent Activity
+                    </h1>
+                    <a
+                        href="/"
+                        className="text-xl font-bold hover:underline dark:text-purple-600"
+                    >
+                        View all
+                    </a>
+                </div>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </div>
+        </Card>
+    )
 }
 
 export default RecentActivity;
--- src/components/dashboardComponents/Stats.js
+++ src/components/dashboardComponents/Stats.js
@@ -1,6 +1,26 @@
+import { Card } from "flowbite-react/lib/cjs/components/Card/Card";
 
 const Stats = () => {
-
+    return (
+        <Card className={"dark:border-4 dark:border-purple-600 dark:bg-stone-900 place-content-center col-span-3"}>
+            <div className="flex flex-col">
+                <div className="mb-4 flex justify-between">
+                    <h1 className="text-3xl font-bold leading-none text-gray-900 dark:text-white">
+                        Stats
+                    </h1>
+                    <a
+                        href="/"
+                        className="text-xl font-bold hover:underline dark:text-purple-600"
+                    >
+                        View all
+                    </a>
+                </div>
+                <p className="font-normal text-white">
+                Here is why ChainSplit is a really good app.
+                </p>
+            </div>
+        </Card>
+    )
 }
 
 export default Stats;
