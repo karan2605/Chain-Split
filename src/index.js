b5b9c2586bdf27affed228260e6947f727fefd0a
Karan Patel <karan2605@hotmail.co.uk>
2022-11-15 12:50:06 +0000

    Added react router tags for navigation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Dashboard from './components/Dashboard';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<App />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
