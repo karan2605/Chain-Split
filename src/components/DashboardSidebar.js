f10984a803f2731a5e5f7ff8c863db09466b64f9
Karan Patel <karan2605@hotmail.co.uk>
2022-11-15 14:25:37 +0000

    Added icons to sidebar

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -1,35 +1,45 @@
 import { Sidebar } from "flowbite-react/lib/cjs/components/Sidebar/Sidebar";
+import { HiHome } from "react-icons/hi2/";
+import { HiUserGroup } from "react-icons/hi2";
+import { HiUsers } from "react-icons/hi2";
+import { HiClipboard } from "react-icons/hi2";
 
 const DashboardSidebar = () => {
     return (
-        <div className="w-fit bg-stone-800">
-            <Sidebar className="bg-stone-800">
-                <Sidebar.Items>
-                <Sidebar.ItemGroup>
+        <Sidebar className="w-fit row-span-3 bg-stone-800 text-4xl">
+            <Sidebar.Items className="bg-stone-800">
+                <Sidebar.ItemGroup className="bg-stone-800">
                     <Sidebar.Item
                     href="#"
+                    className="text-2xl text-white"
+                    icon={HiHome}
                     >
                     Dashboard
                     </Sidebar.Item>
                     <Sidebar.Item
                     href="#"
+                    className=""
+                    icon={HiUsers}
                     >
-                    Friends
+                    <h1 className="text-2xl text-white">Friends</h1>
                     </Sidebar.Item>
                     <Sidebar.Item
                     href="#"
+                    className="text-2xl text-white"
+                    icon={HiUserGroup}
                     >
                     Groups
                     </Sidebar.Item>
                     <Sidebar.Item
                     href="#"
+                    className="text-2xl text-white"
+                    icon={HiClipboard}
                     >
                     Split History
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
-                </Sidebar.Items>
-            </Sidebar>
-        </div>
+            </Sidebar.Items>
+        </Sidebar>
     )
 }
 
