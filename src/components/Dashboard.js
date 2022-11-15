3ffffe3eba54c489ef3e15920f8c91d6ee65801a
Karan Patel <karan2605@hotmail.co.uk>
2022-11-15 12:50:56 +0000

    Dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,14 @@
+import DashboardSidebar from "./DashboardSidebar"; 
+import DashboardBar from "./DashboardBar";
 
 const Dashboard = () => {
-    
+    return (
+        <div className="flex flex-col bg-stone-700">
+            <DashboardBar />
+            <DashboardSidebar className="bg-stone-800"/>
+
+        </div>
+    )
 }
 
 export default Dashboard;
