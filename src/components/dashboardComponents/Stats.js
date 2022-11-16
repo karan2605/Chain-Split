4107125b3009a97f1a47e1d75e95321bc6991fc0
Karan Patel <karan2605@hotmail.co.uk>
2022-11-16 17:51:03 +0000

    Added stats cards

--- src/components/dashboardComponents/Stats.js
+++ src/components/dashboardComponents/Stats.js
@@ -5,7 +5,7 @@ const Stats = () => {
         <Card className={"dark:border-4 dark:border-purple-600 dark:bg-stone-900 place-content-center col-span-3"}>
             <div className="flex flex-col">
                 <div className="mb-4 flex justify-between">
-                    <h1 className="text-3xl font-bold leading-none text-gray-900 dark:text-white">
+                    <h1 className="text-4xl font-bold leading-none text-gray-900 dark:text-white">
                         Stats
                     </h1>
                     <a
@@ -15,9 +15,33 @@ const Stats = () => {
                         View all
                     </a>
                 </div>
-                <p className="font-normal text-white">
-                Here is why ChainSplit is a really good app.
-                </p>
+                <div className="flex flex-row gap-4 items-center justify-evenly">
+                    <Card className={"h-96 text-center"}>
+                        <h5 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Total Received
+                        </h5>
+                        <h1 className="text-5xl font-bold tracking-tight text-green-500">
+                        0.5 ETH
+                        </h1>
+                    </Card>
+                    <Card className={"h-96 text-center"}>
+                        <h5 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Total Contributed
+                        </h5>
+                        <h1 className="text-5xl font-bold tracking-tight text-red-600">
+                        0.4 ETH
+                        </h1>
+                    </Card>
+                    <Card className={"h-96 text-center"}>
+                        <h5 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Number of Active Splits
+                        </h5>
+                        <h1 className="text-5xl font-bold tracking-tight text-blue-500">
+                        6
+                        </h1>
+                    </Card>
+                    
+                </div>
             </div>
         </Card>
     )
