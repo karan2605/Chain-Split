ada2f8899af34453c370022b01a1bc99c84e26fa
Karan Patel <karan2605@hotmail.co.uk>
2022-11-16 17:51:45 +0000

    Added table for recent activity

--- src/components/dashboardComponents/RecentActivity.js
+++ src/components/dashboardComponents/RecentActivity.js
@@ -1,11 +1,13 @@
 import { Card } from "flowbite-react/lib/cjs/components/Card/Card";
+import { Table } from "flowbite-react/lib/cjs/components/Table/Table";
+import { Badge } from "flowbite-react/lib/cjs/components/Badge/Badge";
 
 const RecentActivity = () => {
     return (
         <Card className={"dark:border-4 dark:border-purple-600 dark:bg-stone-900"}>
             <div className="flex flex-col">
                 <div className="mb-4 flex justify-between">
-                    <h1 className="text-3xl font-bold leading-none text-gray-900 dark:text-white">
+                    <h1 className="text-4xl font-bold leading-none text-gray-900 dark:text-white">
                         Recent Activity
                     </h1>
                     <a
@@ -15,9 +17,141 @@ const RecentActivity = () => {
                         View all
                     </a>
                 </div>
-                <p className="font-normal text-white">
-                Here is why ChainSplit is a really good app.
-                </p>
+                <Table striped={true} className="text-md">
+                    <Table.Head>
+                        <Table.HeadCell>
+                        From
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                        To
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                        Amount(ETH)
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                        <span className="sr-only">
+                            View TxN
+                        </span>
+                        </Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body className="divide-y">
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            0.005
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                            href="/tables"
+                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                            View TxN
+                            </a>
+                        </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            0.007
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                            href="/tables"
+                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                            View TxN
+                            </a>
+                        </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            0.002
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                            href="/tables"
+                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                            View TxN
+                            </a>
+                        </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            0.015
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                            href="/tables"
+                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                            View TxN
+                            </a>
+                        </Table.Cell>
+                        </Table.Row>
+                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Badge color="purple" size="lg">
+                                0x213ef...j2nk
+                            </Badge>
+                        </Table.Cell>
+                        <Table.Cell>
+                            0.025
+                        </Table.Cell>
+                        <Table.Cell>
+                            <a
+                            href="/tables"
+                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >
+                            View TxN
+                            </a>
+                        </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
             </div>
         </Card>
     )
