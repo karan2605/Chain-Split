088fbcb4bddef846ce8735d49a011c307daa87cc
Karan Patel <karan2605@hotmail.co.uk>
2022-11-20 09:55:57 +0000

    Removed title and view all link

--- src/components/dashboardComponents/Stats.js
+++ src/components/dashboardComponents/Stats.js
@@ -4,17 +4,6 @@ const Stats = () => {
     return (
         <Card className={"dark:border-4 dark:border-purple-600 dark:bg-stone-900 place-content-center col-span-3"}>
             <div className="flex flex-col">
-                <div className="mb-4 flex justify-between">
-                    <h1 className="text-4xl font-bold leading-none text-gray-900 dark:text-white">
-                        Stats
-                    </h1>
-                    <a
-                        href="/"
-                        className="text-xl font-bold hover:underline dark:text-purple-600"
-                    >
-                        View all
-                    </a>
-                </div>
                 <div className="flex flex-row gap-4 items-center justify-evenly">
                     <Card className={"h-96 text-center"}>
                         <h5 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
