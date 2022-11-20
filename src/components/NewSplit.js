1f7aa1908e7355a28a43e7c55170a80060212a5e
Karan Patel <karan2605@hotmail.co.uk>
2022-11-20 19:55:56 +0000

    Added form into modal

--- src/components/NewSplit.js
+++ src/components/NewSplit.js
@@ -1,38 +1,57 @@
-import React, { useState } from 'react';
-
 import { Modal } from "flowbite-react/lib/cjs/components/Modal";
 import { Button } from "flowbite-react/lib/cjs/components/Button/Button";
+import { Label, Checkbox, TextInput, Dropdown } from "flowbite-react";
 
-const NewSplit = ({toggle}) => {
-
-    const [onClose, setClose] = useState(false);
-
-    return (
-        <Modal
-            show={true}
-            position="center"
-            onClose={toggle}
-        >
-            <Modal.Header>
-            Start a New Split
-            </Modal.Header>
-            <Modal.Body>
-            <div className="space-y-6">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
+const NewSplit = ({ toggle }) => {
+  return (
+    <Modal show={true} position="center" onClose={toggle}>
+      <Modal.Header>Start a New Split</Modal.Header>
+      <Modal.Body>
+        <form className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="groupName" value="Group Name" />
             </div>
-            </Modal.Body>
-            <Modal.Footer>
-            <Button onClick={toggle}>
-                I accept
-            </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+            <TextInput id="groupName" type="text" required={true} />
+          </div>
+          <div>
+            <Dropdown label="Select Token" dismissOnClick={false} color="purple">
+              <Dropdown.Item className="flex gap-4">Token 1</Dropdown.Item>
+              <Dropdown.Item className="flex gap-4">Token 2</Dropdown.Item>
+              <Dropdown.Item className="flex gap-4">Token 3</Dropdown.Item>
+              <Dropdown.Item className="flex gap-4">Token 4</Dropdown.Item>
+              <Dropdown.Item className="flex gap-4">Token 5</Dropdown.Item>
+            </Dropdown>
+          </div>
+          <div>
+            <label for="friends_multiple" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Friends</label>
+            <select multiple id="friends_multiple" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="0x213ef...j2nk">Friend 1</option>
+            <option value="0x213ef...j2nk">Friend 2</option>
+            <option value="0x213ef...j2nk">Friend 3</option>
+            <option value="0x213ef...j2nk">Friend 4</option>
+            </select>
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="amount" value="Total Amount" />
+            </div>
+            <TextInput id="amount" type="number" required={true} placeholder="> 0" className="font-mono"/>
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="initContrib" value="Your Contribution" />
+            </div>
+            <TextInput id="initContrib" type="number" required={true} placeholder=">= 0" className="font-mono"/>
+          </div>
+          <Button type="submit" color="purple">Confirm</Button>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Label value="NOTE - Once confirmed the transaction is irreversible" class={"text-red-700 font-bold"}/>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default NewSplit;
