e1ec2d132f9b28a0c4957025f06f02d7a4e78c2c
Karan Patel <karan2605@hotmail.co.uk>
2022-11-21 11:12:57 +0000

    allow split initiator to add in amount needed for each depositor

--- src/components/NewSplit.js
+++ src/components/NewSplit.js
@@ -1,8 +1,16 @@
-import { Label, TextInput, Dropdown, Button, Modal } from "flowbite-react";
+import {
+  Label,
+  TextInput,
+  Dropdown,
+  Button,
+  Modal,
+  Table,
+  Checkbox,
+} from "flowbite-react";
 
 const NewSplit = ({ toggle }) => {
   return (
-    <Modal show={true} position="center" onClose={toggle}>
+    <Modal show={true} position="center" onClose={toggle} size="4xl">
       <Modal.Header>Start a New Split</Modal.Header>
       <Modal.Body>
         <form className="flex flex-col gap-4">
@@ -13,7 +21,11 @@ const NewSplit = ({ toggle }) => {
             <TextInput id="groupName" type="text" required={true} />
           </div>
           <div>
-            <Dropdown label="Select Token" dismissOnClick={false} color="purple">
+            <Dropdown
+              label="Select Token"
+              dismissOnClick={false}
+              color="purple"
+            >
               <Dropdown.Item className="flex gap-4">Token 1</Dropdown.Item>
               <Dropdown.Item className="flex gap-4">Token 2</Dropdown.Item>
               <Dropdown.Item className="flex gap-4">Token 3</Dropdown.Item>
@@ -22,31 +34,140 @@ const NewSplit = ({ toggle }) => {
             </Dropdown>
           </div>
           <div>
-            <label for="friends_multiple" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Friends</label>
-            <select multiple id="friends_multiple" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="0x213ef...j2nk">Friend 1</option>
-            <option value="0x213ef...j2nk">Friend 2</option>
-            <option value="0x213ef...j2nk">Friend 3</option>
-            <option value="0x213ef...j2nk">Friend 4</option>
-            </select>
+            <Table>
+              <Table.Head>
+                <Table.HeadCell>Friends</Table.HeadCell>
+                <Table.HeadCell>Select for this Split</Table.HeadCell>
+                <Table.HeadCell>Amount Required</Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Friend 1
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <TextInput
+                      id="amount"
+                      type="number"
+                      size="sm"
+                      required={true}
+                      placeholder="> 0"
+                      className="font-mono"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Friend 2
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <TextInput
+                      id="amount"
+                      type="number"
+                      size="sm"
+                      required={true}
+                      placeholder="> 0"
+                      className="font-mono"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Friend 3
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <TextInput
+                      id="amount"
+                      type="number"
+                      size="sm"
+                      required={true}
+                      placeholder="> 0"
+                      className="font-mono"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Friend 4
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <TextInput
+                      id="amount"
+                      type="number"
+                      size="sm"
+                      required={true}
+                      placeholder="> 0"
+                      className="font-mono"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    Friend 5
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <TextInput
+                      id="amount"
+                      type="number"
+                      size="sm"
+                      required={true}
+                      placeholder="> 0"
+                      className="font-mono"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </div>
           <div>
             <div className="mb-2 block">
               <Label htmlFor="amount" value="Total Amount" />
             </div>
-            <TextInput id="amount" type="number" required={true} placeholder="> 0" className="font-mono"/>
+            <TextInput
+              id="amount"
+              type="number"
+              required={true}
+              placeholder="> 0"
+              className="font-mono"
+            />
           </div>
           <div>
             <div className="mb-2 block">
               <Label htmlFor="initContrib" value="Your Contribution" />
             </div>
-            <TextInput id="initContrib" type="number" required={true} placeholder=">= 0" className="font-mono"/>
+            <TextInput
+              id="initContrib"
+              type="number"
+              required={true}
+              placeholder=">= 0"
+              className="font-mono"
+            />
           </div>
-          <Button type="submit" color="purple">Confirm</Button>
+          <Button type="submit" color="purple">
+            Confirm
+          </Button>
         </form>
       </Modal.Body>
       <Modal.Footer>
-        <Label value="NOTE - Once confirmed the transaction is irreversible" class={"text-red-700 font-bold"}/>
+        <Label
+          value="NOTE - Once confirmed the transaction is irreversible"
+          class={"text-red-700 font-bold"}
+        />
       </Modal.Footer>
     </Modal>
   );
