c4eb354702eb7dbc77f3f756d465ff6949a73173
Karan Patel <karan2605@hotmail.co.uk>
2022-11-24 17:21:05 +0000

    Load account

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
 import { Button } from "flowbite-react/lib/cjs/components/Button";
 
 import SiteFooter from "./components/SiteFooter";
@@ -5,25 +8,44 @@ import HomeInfo from "./components/HomeInfo";
 import NavigationBar from "./components/Navbar";
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div className="flex flex-col bg-stone-800 ">
-      <NavigationBar />
+      <NavigationBar account={account} setAccount={setAccount} />
 
       <div class="w-full h-96 bg-gradient-to-r from-blue-600 to-fuchsia-600">
         <div class="flex flex-col items-center justify-center h-full">
-          <h1 class="text-white text-6xl font-semibold p-6">A New Way To Split Your Expenses</h1>
-          <Button gradientDuoTone="purpleToPink" size="xl" className="font-semibold text-4xl">
-            <span className="text-3xl">
-              Get Started
-            </span>
+          <h1 class="text-white text-6xl font-semibold p-6">
+            A New Way To Split Your Expenses
+          </h1>
+          <Button
+            gradientDuoTone="purpleToPink"
+            size="xl"
+            className="font-semibold text-4xl"
+          >
+            <span className="text-3xl">Get Started</span>
           </Button>
         </div>
       </div>
-      
+
       <HomeInfo />
-      
-      <SiteFooter/>
-    
+
+      <SiteFooter />
     </div>
   );
 }
