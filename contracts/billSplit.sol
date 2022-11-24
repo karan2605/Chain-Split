97806148b4e00434881eedfdf6df5cbb82c58aff
Karan Patel <karan2605@hotmail.co.uk>
2022-11-24 16:38:54 +0000

    moved split attributes out of the constructor

--- contracts/billSplit.sol
+++ contracts/billSplit.sol
@@ -14,20 +14,23 @@ contract BillSplit is ReentrancyGuard {
     address payable public initiator;
     address payable[] public depositors;
 
-    mapping (address => uint) public depositorOwes;
-    mapping (address => bool) private depositorExists;
+    mapping(address => uint) public depositorOwes;
+    mapping(address => bool) private depositorExists;
     address public deployer;
     address private tokenAddress;
 
     ERC20 public token;
 
-    modifier OnlyInitiator {
+    modifier OnlyInitiator() {
         require(msg.sender == initiator, "Only initiator can begin split");
         _;
     }
 
     modifier OnlyDepositor(address _caller) {
-        require(depositorExists[_caller], "Only the depositor can add their split amount");
+        require(
+            depositorExists[_caller],
+            "Only the depositor can add their split amount"
+        );
         _;
     }
 
@@ -43,66 +46,76 @@ contract BillSplit is ReentrancyGuard {
     // Event emitted when the split has been completed
     event SplitCompleted(address initiator, uint256 totalAmount);
 
-    constructor (
-        uint256 _totalAmount,
-        address payable _initiator,
-        address payable[] memory _depositors,
-        address payable _deployer,
-        address _tokenAddress
-    ) {
-        totalAmount = _totalAmount;
-        initiator = _initiator;
-        depositors = _depositors;
-
-        for(uint i = 0; i < _depositors.length; i++) {
-            depositorOwes[_depositors[i]] = 0;
-            depositorExists[_depositors[i]] = true;
-        }
-
+    constructor(address payable _deployer, address _tokenAddress) {
         deployer = _deployer;
         token = ERC20(_tokenAddress);
         tokenAddress = _tokenAddress;
     }
 
-    function initiateSplit(uint256 _initiatorAmt, uint256[] memory _depositorAmts) external OnlyInitiator {
+    function initiateSplit(
+        uint256 _totalAmount,
+        address payable _initiator,
+        uint256 _initiatorAmt,
+        address payable[] memory _depositors,
+        uint256[] memory _depositorAmts
+    ) external OnlyInitiator {
+        depositors = _depositors;
+        totalAmount = _totalAmount;
+        initiator = _initiator;
+
+        for (uint i = 0; i < _depositors.length; i++) {
+            depositorOwes[_depositors[i]] = 0;
+            depositorExists[_depositors[i]] = true;
+        }
+
         uint256 depositorTotal = 0;
 
         require(_initiatorAmt >= 0, "Initiator amount must be non-negative");
 
         // Calculates the total amount that must be given by all depositors
         // Adds entry to mapping indicating how much each account owes
-        for(uint i = 0; i < _depositorAmts.length; i++) {
+        for (uint i = 0; i < _depositorAmts.length; i++) {
             depositorTotal += _depositorAmts[i];
-            require(_depositorAmts[i] >= 0, "Depositor amount must be non-negative");
+            require(
+                _depositorAmts[i] >= 0,
+                "Depositor amount must be non-negative"
+            );
             depositorOwes[depositors[i]] = _depositorAmts[i];
         }
 
-        require(_initiatorAmt + depositorTotal == totalAmount, "Split does not equal total amount owed.");
-    
+        require(
+            _initiatorAmt + depositorTotal == totalAmount,
+            "Split does not equal total amount owed."
+        );
+
         // Transfer depositors tokens into contract
         token.transferFrom(deployer, address(this), _initiatorAmt);
-        require(token.balanceOf(address(this)) == _initiatorAmt, "Initiators split not transferred");
+        require(
+            token.balanceOf(address(this)) == _initiatorAmt,
+            "Initiators split not transferred"
+        );
         emit SplitInitiated(initiator, totalAmount);
     }
 
     function split() external OnlyDepositor(msg.sender) {
-
         // Check balance before TxN
         uint256 balanceBefore = token.balanceOf(address(this));
 
         // Transfer depositors tokens into contract
         token.transferFrom(deployer, address(this), depositorOwes[msg.sender]);
-        
+
         // Assert account balance equals total
         uint256 balance = token.balanceOf(address(this));
-        require(balance == depositorOwes[msg.sender] + balanceBefore, "Funds not received from depositor");
+        require(
+            balance == depositorOwes[msg.sender] + balanceBefore,
+            "Funds not received from depositor"
+        );
 
         // Emit an event indicating the depositor has sent money to the initiator through the contract
         emit DepositReceived(msg.sender, depositorOwes[msg.sender]);
     }
 
     function transferTotal() external OnlyInitiator {
-
         // Transfer account balance to initiator
         token.transfer(initiator, token.balanceOf(address(this)));
 
