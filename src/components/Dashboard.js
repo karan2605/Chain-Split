e91ec038807381d073fa819419c3f7cfc7b1bddc
Karan Patel <karan2605@hotmail.co.uk>
2022-11-26 12:17:02 +0000

    Added connect handler and sent account data as props into dashboard bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,6 @@
+import { ethers } from 'ethers';
+import { useState, useEffect } from 'react';
+
 import DashboardSidebar from "./DashboardSidebar";
 import DashboardBar from "./DashboardBar";
 import SiteFooter from "./SiteFooter";
@@ -7,9 +10,21 @@ import PendingSplits from "./dashboardComponents/PendingSplits";
 import Stats from "./dashboardComponents/Stats";
 
 const Dashboard = () => {
+  const [account, setAccount] = useState(null)
+
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    connectHandler()
+  })
+
   return (
     <div className="flex flex-col">
-      <DashboardBar />
+      <DashboardBar account={account}/>
       <div className="flex flex-row">
         <DashboardSidebar />
         <div className="grid grid-rows-2 grid-cols-3 h-screen bg-stone-900 gap-6 px-20 py-20 w-full">
