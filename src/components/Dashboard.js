a962a879f6b9cc0b19adbc828ea7f5ae75559688
Karan Patel <karan2605@hotmail.co.uk>
2022-11-26 17:43:25 +0000

    boilerplate to recieve data from IPFS

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,8 +9,30 @@ import Friends from "./dashboardComponents/Friends";
 import PendingSplits from "./dashboardComponents/PendingSplits";
 import Stats from "./dashboardComponents/Stats";
 
+import { Web3Storage, File } from "web3.storage";
+
 const Dashboard = () => {
   const [account, setAccount] = useState(null)
+
+  function getAccessToken() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGE0RjI0MjVkMGVGZjE5QmFFZDc1YzA3ZTNENEJiNDI4MTdiZDYzZGYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjkzNzA0NTA0NjIsIm5hbWUiOiJDaGFpblNwbGl0In0.IniPPZENlFLjDWi4_tAwgc67THksBDYTcSrCYR2kj28";
+  }
+
+  function makeStorageClient() {
+    return new Web3Storage({ token: getAccessToken() });
+  }
+
+  async function retrieve (cid) {
+    const client = makeStorageClient()
+    const res = await client.get(cid)
+    console.log(`Got a response! [${res.status}] ${res.statusText}`)
+    if (!res.ok) {
+      throw new Error(`failed to get ${cid}`)
+    }
+  
+    // request succeeded! do something with the response object here...
+  }
+
 
   const connectHandler = async () => {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -21,6 +43,8 @@ const Dashboard = () => {
   useEffect(() => {
     connectHandler()
   })
+
+
 
   return (
     <div className="flex flex-col">
