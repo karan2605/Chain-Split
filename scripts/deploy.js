fb003249f23c4eabc0a62cc22b93776d63439c7e
Karan Patel <karan2605@hotmail.co.uk>
2022-11-29 13:05:16 +0000

    removed dead code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,6 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const { ethers } = require("hardhat");
-const hre = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
