027db1f9f6239e685b8641f7952e08cf6bee5f60
Karan Patel <karan2605@hotmail.co.uk>
2022-12-07 20:10:06 +0000

    code formatting + added a function to get account data

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -1,8 +1,8 @@
 import { Web3Storage, File } from "web3.storage";
 
-import config from '../config.json';
-import Account from "../abis/Account.json"
-import BillSplit from "../abis/BillSplit.json"
+import config from "../config.json";
+import Account from "../abis/Account.json";
+import BillSplit from "../abis/BillSplit.json";
 
 import { ethers } from "ethers";
 import { useState } from "react";
@@ -13,74 +13,128 @@ function getAccessToken() {
 
 const makeStorageClient = () => {
   return new Web3Storage({ token: getAccessToken() });
+};
+
+const UploadToIPFS = async (account, data) => {
+  const files = [new File([data], account + ".json")];
+  const cid = await StoreFiles(files);
+
+  const prov = new ethers.providers.Web3Provider(window.ethereum);
+
+  const network = await prov.getNetwork();
+  const acc = new ethers.Contract(
+    config[network.chainId].Account.address,
+    Account,
+    prov
+  );
+
+  const signer = prov.getSigner();
+
+  let transaction = await acc.connect(signer).setHash(cid);
+  await transaction.wait();
+};
+
+const GetAccountData = async () => {
+  const prov = new ethers.providers.Web3Provider(window.ethereum);
+  const network = await prov.getNetwork();
+  const account = new ethers.Contract(
+    config[network.chainId].Account.address,
+    Account,
+    prov
+  );
+  const signer = prov.getSigner();
+
+  let cid = await account.connect(signer).getHash();
+  const client = makeStorageClient();
+  let res = await client.get(cid);
+  console.log(`Got a response! [${res.status}] ${res.statusText}`);
+  if (!res.ok) {
+    throw new Error(`failed to get ${cid}`);
+  }
+  const files = await res.files();
+  return JSON.parse(await files[0].text());
+};
+
+async function StoreFiles(files) {
+  const client = makeStorageClient();
+  const cid = await client.put(files);
+  console.log("stored files with cid:", cid);
+  return cid;
 }
 
-const UploadToIPFS = async ({ account, data }) => {
-    const [provider, setProvider] = useState(null)
+const InitiateSplit = async (
+  totalAmount,
+  initiatorAmt,
+  depositors,
+  depositorAmts
+) => {
+  const [provider, setProvider] = useState(null);
+  const prov = new ethers.providers.Web3Provider(window.ethereum);
+  setProvider(prov);
 
-    const files = [new File([data], account+".json")];
+  const network = await prov.getNetwork();
+  const acc = new ethers.Contract(
+    config[network.chainId].BillSplit.address,
+    BillSplit,
+    provider
+  );
 
-    const cid = await StoreFiles(files);
+  const signer = await provider.getSigner();
 
-    const prov = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(prov)
-    const network = await prov.getNetwork()
-    const acc = new ethers.Contract(config[network.chainId].Account.address, Account, provider)
-
-    const signer = await provider.getSigner()
-
-    let transaction = await acc.connect(signer).setHash(cid)
-    await transaction.wait()
-}
-
-const StoreFiles = async ({ files }) => {
-    const client = makeStorageClient();
-    const cid = await client.put(files);
-    console.log("stored files with cid:", cid);
-    return cid;
-}
-
-const InitiateSplit = async ({ totalAmount, initiatorAmt, depositors, depositorAmts }) => {
-  const [provider, setProvider] = useState(null)
-  const prov = new ethers.providers.Web3Provider(window.ethereum)
-  setProvider(prov)
-
-  const network = await prov.getNetwork()
-  const acc = new ethers.Contract(config[network.chainId].BillSplit.address, BillSplit, provider)
-
-  const signer = await provider.getSigner()
-
-  let transaction = await acc.connect(signer).initiateSplit(totalAmount, provider, initiatorAmt, depositors, depositorAmts)
-  await transaction.wait()
-}
+  let transaction = await acc
+    .connect(signer)
+    .initiateSplit(
+      totalAmount,
+      provider,
+      initiatorAmt,
+      depositors,
+      depositorAmts
+    );
+  await transaction.wait();
+};
 
 const Split = async () => {
-  const [provider, setProvider] = useState(null)
-  const prov = new ethers.providers.Web3Provider(window.ethereum)
-  setProvider(prov)
+  const [provider, setProvider] = useState(null);
+  const prov = new ethers.providers.Web3Provider(window.ethereum);
+  setProvider(prov);
 
-  const network = await prov.getNetwork()
-  const acc = new ethers.Contract(config[network.chainId].BillSplit.address, BillSplit, provider)
+  const network = await prov.getNetwork();
+  const acc = new ethers.Contract(
+    config[network.chainId].BillSplit.address,
+    BillSplit,
+    provider
+  );
 
-  const signer = await provider.getSigner()
+  const signer = await provider.getSigner();
 
-  let transaction = await acc.connect(signer).split()
-  await transaction.wait()
-}
+  let transaction = await acc.connect(signer).split();
+  await transaction.wait();
+};
 
 const TransferTotal = async () => {
-  const [provider, setProvider] = useState(null)
-  const prov = new ethers.providers.Web3Provider(window.ethereum)
-  setProvider(prov)
+  const [provider, setProvider] = useState(null);
+  const prov = new ethers.providers.Web3Provider(window.ethereum);
+  setProvider(prov);
 
-  const network = await prov.getNetwork()
-  const acc = new ethers.Contract(config[network.chainId].BillSplit.address, BillSplit, provider)
+  const network = await prov.getNetwork();
+  const acc = new ethers.Contract(
+    config[network.chainId].BillSplit.address,
+    BillSplit,
+    provider
+  );
 
-  const signer = await provider.getSigner()
+  const signer = await provider.getSigner();
 
-  let transaction = await acc.connect(signer).TransferTotal()
-  await transaction.wait()
-}
+  let transaction = await acc.connect(signer).TransferTotal();
+  await transaction.wait();
+};
 
-export { UploadToIPFS, makeStorageClient, StoreFiles, InitiateSplit, Split, TransferTotal }
-
+export {
+  UploadToIPFS,
+  makeStorageClient,
+  StoreFiles,
+  InitiateSplit,
+  Split,
+  TransferTotal,
+  GetAccountData,
+};
