00b09a0734267328ba76a11047880bd264013eec
Karan Patel <karan2605@hotmail.co.uk>
2022-12-08 19:36:25 +0000

    added correct method arguments and await modifier

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -2,38 +2,48 @@ import { Label, TextInput, Button, Modal, Alert } from "flowbite-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-import { UploadToIPFS } from "./Utilities";
+import { UploadToIPFS, GetAccountData } from "./Utilities";
 
-const AddFriend = ({ toggle, globalData }) => {
-  const [showAlert, setShowAlert] = useState(false)
-  const { reset } = useForm()
+const AddFriend = ({ toggle }) => {
+  const [showAlert, setShowAlert] = useState(false);
+  const { reset } = useForm();
 
   const addFriend = async (event) => {
     event.preventDefault();
 
-    const friendName = event.target[0].value
-    const friendAccount = event.target[1].value
+    const data = await GetAccountData();
+    console.log(data)
 
-    globalData.friends = globalData.friends.push({account : friendAccount, name : friendName})
+    const friendName = event.target[0].value;
+    const friendAccount = event.target[1].value;
 
-    UploadToIPFS(globalData)
+    data.friends = data.friends.push({
+      account: friendAccount,
+      name: friendName,
+    });
 
-    setShowAlert(true)
-    reset()
-  }
+    await UploadToIPFS(data.account, data);
+
+    setShowAlert(true);
+    reset();
+  };
 
   return (
     <Modal show={true} position="center" onClose={toggle}>
-      {showAlert && <Alert
-        color="success"
-        onDismiss={function onDismiss() {
-          setShowAlert(false)
-        }}
-      >
-        <span>
-          <span className="font-medium">Account Created! You can now log in by connecting to MetaMask</span> 
-        </span>
-      </Alert> }
+      {showAlert && (
+        <Alert
+          color="success"
+          onDismiss={function onDismiss() {
+            setShowAlert(false);
+          }}
+        >
+          <span>
+            <span className="font-medium">
+              Account Created! You can now log in by connecting to MetaMask
+            </span>
+          </span>
+        </Alert>
+      )}
       <Modal.Header>Add New Friend</Modal.Header>
       <Modal.Body>
         <form className="flex flex-col gap-4" onSubmit={addFriend}>
@@ -47,7 +57,12 @@ const AddFriend = ({ toggle, globalData }) => {
             <div className="mb-2 block">
               <Label htmlFor="accHash" value="Public Key" />
             </div>
-            <TextInput id="accHash" type="text" required={true} className={"font-mono"}/>
+            <TextInput
+              id="accHash"
+              type="text"
+              required={true}
+              className={"font-mono"}
+            />
           </div>
           <Button type="submit" color="purple">
             Add Friend
@@ -57,7 +72,7 @@ const AddFriend = ({ toggle, globalData }) => {
       <Modal.Footer>
         <Label
           value="NOTE - Once confirmed the transaction is irreversible"
-          class={"text-red-700 font-bold"}
+          className={"text-red-700 font-bold"}
         />
       </Modal.Footer>
     </Modal>
