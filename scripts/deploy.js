f86a91d09569a9c7350c3ecd739687a22db58f7a
Karan Patel <karan2605@hotmail.co.uk>
2022-12-16 17:53:10 +0000

    approve billsplit contract and accounts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,31 +7,50 @@
 const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+  let accounts = await ethers.getSigners();
+  let deployer = accounts[0];
 
   // Deploy ERC20
-  const ERC20 = await ethers.getContractFactory('ERC20')
-  const token = await ERC20.deploy('Ether', 'ETH', tokens(1000000000000))
-  await token.deployed()
+  const ERC20 = await ethers.getContractFactory("ERC20");
+  const token = await ERC20.deploy("Ether", "ETH", tokens(1000000000000));
+  await token.deployed();
 
-  console.log(`Deployed ERC20 Contract at : ${token.address}`)
+  console.log(`Deployed ERC20 Contract at : ${token.address}`);
 
-  const BillSplit = await ethers.getContractFactory("BillSplit")
-  const billSplit = await BillSplit.deploy(token.address)
-  await billSplit.deployed()
+  const BillSplit = await ethers.getContractFactory("BillSplit");
+  const billSplit = await BillSplit.deploy(token.address);
+  await billSplit.deployed();
 
-  console.log(`Deployed BillSplit Contract at : ${billSplit.address}`)
+  console.log(`Deployed BillSplit Contract at : ${billSplit.address}`);
 
-  const Account = await ethers.getContractFactory("Account")
-  const account = await Account.deploy()
-  await account.deployed()
+  const Account = await ethers.getContractFactory("Account");
+  const account = await Account.deploy();
+  await account.deployed();
 
-  console.log(`Deployed Account Contract at : ${account.address}`)
-  console.log(`Finished.`)
+  console.log(`Deployed Account Contract at : ${account.address}`);
 
+  console.log(`Approving BillSplit contract from ERC20`)
+  // Approve contract account
+  let transaction = await token
+    .connect(deployer)
+    .approve(billSplit.address, 9999999999999);
+  await transaction.wait();
+  console.log(`BillSplit Approved`)
+
+  console.log(`Approving Accounts`)
+  for (let i = 1; i < accounts.length; i++) {
+    transaction = await token
+      .connect(deployer)
+      .approve(accounts[i].address, 999999999999);
+    await transaction.wait();
+  }
+  console.log(`Accounts Approved`)
+
+  console.log(`Finished.`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
