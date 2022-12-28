4344a3615efcd54a9a791d107ab08be95329d98b
Karan Patel <karan2605@hotmail.co.uk>
2022-12-28 18:01:26 +0000

    contract now allows for multiple split groups

--- contracts/SplitExpenses.sol
+++ contracts/SplitExpenses.sol
@@ -2,117 +2,130 @@
 
 pragma solidity ^0.8.0;
 
-contract SplitExpenses {
-    // mapping of address to their balance in the contract
-    mapping(address => uint) public balances;
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-    // mapping of address to their total contribution to the contract
-    mapping(address => uint) public contributions;
+contract SplitExpenses is ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for ERC20;
 
-    // mapping of group ID to a list of addresses that belong to the group
-    mapping(bytes32 => address[]) public groups;
+    address public deployer;
 
-    // mapping of group ID to a list of intended contributions for each member of the group
-    mapping(bytes32 => uint[]) public intendedContributions;
+    mapping(string => Group) public groups;
 
-    // event for when a contribution is made
-    event Contribution(address sender, uint amount, bytes32 group);
+    mapping(string => uint256) contractBalances;
 
-    // owner of the contract
-    address public owner;
+    ERC20 public token;
 
-    // boolean flag to indicate if the contract is stopped
-    bool public stopped;
-
-    // constructor to set the owner of the contract
-    constructor() {
-        owner = msg.sender;
+    struct Group {
+        // The ID of the group
+        string id;
+        // The addresses of the members of the group
+        address[] members;
+        // The total amount of expenses or bills for the group
+        uint256 totalAmount;
+        // The mapping of member addresses to their contribution amounts
+        mapping(address => uint256) contributions;
+        // The mapping of member addresses to whether they have approved the expenses or bills
+        mapping(address => bool) approvals;
     }
 
-    // modifier to restrict a function to the contract owner
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Event when a split has been initiated
+    event SplitInitiated(address initiator, uint256 total);
+
+    // Event emitted when a depositer has sent tokens to the contract
+    event DepositReceived(address sender, uint256 amount);
+
+    // Event emitted when the split has been completed
+    event SplitCompleted(address initiator, uint256 totalAmount);
+
+    constructor(address _tokenAddress, address _deployer) {
+        deployer = _deployer;
+        token = ERC20(_tokenAddress);
     }
 
-    // function to contribute money to the contract
-    function contribute(uint amount, bytes32 group) public {
-        // check if the contract is stopped
-        require(!stopped, "The contract is stopped");
+    function initiateSplit(
+        string memory _name,
+        uint256 _totalAmount,
+        address payable[] memory _members,
+        uint256[] memory _amounts
+    ) external {
+        require(_amounts[0] >= 0, "Initiator amount must be non-negative");
+        uint256 depositorTotal;
 
-        // check if the group exists
-        require(groups[group].length > 0, "Group does not exist");
-
-        // update the contribution and balance of the sender
-        contributions[msg.sender] += amount;
-        balances[msg.sender] += amount;
-
-        // add the sender to the group
-        groups[group].push(msg.sender);
-
-        // add the contribution to the list of intended contributions for the group
-        intendedContributions[group].push(amount);
-
-        // emit the Contribution event
-        emit Contribution(msg.sender, amount, group);
-    }
-
-    // function to split the expenses between the contributors in a group
-    function splitExpenses(bytes32 group) public {
-        // check if the contract is stopped
-        require(!stopped, "The contract is stopped");
-
-        // check if the group exists
-        require(groups[group].length > 0, "Group does not exist");
-
-        // check if the caller is a member of the group
-        require(isMember(msg.sender, group), "You are not a member of this group");
-
-        // iterate over the contributors in the group and update their balance based on their intended contribution
-        for (uint i = 0; i < groups[group].length; i++) {
-            address contributor = groups[group][i];
-            uint contribution = intendedContributions[group][i];
-            balances[contributor] -= contribution;
+        for (uint i = 0; i < _amounts.length; i++) {
+            depositorTotal += _amounts[i];
+            require(_amounts[i] >= 0, "Depositor amount must be non-negative");
         }
-    }
 
-    // function to create a new group
-    function createGroup(bytes32 group) public {
-        // check if the contract is stopped
-        require(!stopped, "The contract is stopped");
+        require(
+            depositorTotal == _totalAmount,
+            "Split does not equal total amount owed."
+        );
 
-        // check if the group already exists
-        require(groups[group].length == 0, "Group already exists");
+        // Create a new group structure and populate its fields
+        groups[_name].id = _name;
+        groups[_name].members = _members;
+        groups[_name].totalAmount = _totalAmount;
 
-        // create the group
-        groups[group].push(msg.sender);
-    }
-
-    // function to withdraw the balance of the caller
-    function withdraw() public {
-        // check if the contract is stopped
-        require(!stopped, "The contract is stopped");
-        // send the balance of the caller to their address
-        payable(msg.sender).transfer(balances[msg.sender]);
-
-        // reset the balance of the caller to zero
-        balances[msg.sender] = 0;
-    }
-
-    // function to stop the contract
-    function emergencyStop() onlyOwner public {
-        // set the stopped flag to true
-        stopped = true;
-    }
-
-    // function to check if an address is a member of a group
-    function isMember(address user, bytes32 group) public view returns (bool) {
-        // iterate over the members of the group and check if the user is a member
-        for (uint i = 0; i < groups[group].length; i++) {
-            if (groups[group][i] == user) {
-                return true;
-            }
+        for (uint i = 0; i < _amounts.length; i++) {
+            groups[_name].contributions[_members[i]] = _amounts[i];
         }
-        return false;
+
+        // Transfer depositors tokens into contract
+        token.transferFrom(deployer, address(this), _amounts[0]);
+        require(
+            token.balanceOf(address(this)) == _amounts[0],
+            "Initiators split not transferred"
+        );
+
+        contractBalances[_name] = _amounts[0];
+        emit SplitInitiated(_members[0], _totalAmount);
+    }
+
+    function contribute(string memory _name) external {
+        // Check balance before TxN
+        uint256 balanceBefore = contractBalances[_name];
+        uint256 contribution = groups[_name].contributions[msg.sender];
+
+        // Transfer depositors tokens into contract
+        token.transferFrom(deployer, address(this), contribution);
+        contractBalances[_name] += contribution;
+
+        // Assert account balance equals total
+        require(
+            contractBalances[_name] == contribution + balanceBefore,
+            "Funds not received from depositor"
+        );
+
+        // Emit an event indicating the depositor has sent money to the initiator through the contract
+        emit DepositReceived(msg.sender, contribution);
+    }
+
+    function transferTotal(string memory _name) external {
+        require(
+            contractBalances[_name] ==
+                groups[_name].totalAmount,
+            "Funds are not ready to be collected yet"
+        );
+        // Check account is initiator
+        require(
+            msg.sender == groups[_name].members[0],
+            "Caller is not split initiator"
+        );
+
+        // Transfer account balance to initiator
+        token.transfer(msg.sender, contractBalances[_name]);
+
+        contractBalances[_name] = 0;
+
+        // Emit an event indicating the depositor has sent money to the initiator through the contract
+        emit SplitCompleted(msg.sender, groups[_name].totalAmount);
     }
 }
