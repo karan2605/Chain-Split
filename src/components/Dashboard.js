0dcc4eb15df6a9f23ad6a7d69ab41f67e4225ece
Karan Patel <karan2605@hotmail.co.uk>
2023-01-01 19:52:09 +0000

    removing functions not needed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,44 +8,8 @@ import Friends from "./dashboardComponents/Friends";
 import PendingSplits from "./dashboardComponents/PendingSplits";
 import Stats from "./dashboardComponents/Stats";
 
-import { Web3Storage } from "web3.storage";
-
-import AccountAddress from "../contractData/Account-address.json";
-import AccountAbi from "../contractData/Account.json";
-
 const Dashboard = ({ globalData }) => {
   const [account, setAccount] = useState(null);
-  const [provider, setProvider] = useState(null);
-
-  function getAccessToken() {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGE0RjI0MjVkMGVGZjE5QmFFZDc1YzA3ZTNENEJiNDI4MTdiZDYzZGYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjkzNzA0NTA0NjIsIm5hbWUiOiJDaGFpblNwbGl0In0.IniPPZENlFLjDWi4_tAwgc67THksBDYTcSrCYR2kj28";
-  }
-
-  function makeStorageClient() {
-    return new Web3Storage({ token: getAccessToken() });
-  }
-
-  async function retrieve() {
-    const prov = new ethers.providers.Web3Provider(window.ethereum);
-    setProvider(prov);
-    
-    const account = new ethers.Contract(
-      AccountAddress.address,
-      AccountAbi.abi,
-      provider
-    );
-    const signer = prov.getSigner();
-
-    let cid = await account.connect(signer).getHash();
-    const client = makeStorageClient();
-    let res = await client.get(cid);
-    console.log(`Got a response! [${res.status}] ${res.statusText}`);
-    if (!res.ok) {
-      throw new Error(`failed to get ${cid}`);
-    }
-    const files = await res.files();
-    globalData = JSON.parse(await files[0].text());
-  }
 
   const connectHandler = async () => {
     const accounts = await window.ethereum.request({
@@ -57,7 +21,6 @@ const Dashboard = ({ globalData }) => {
 
   useEffect(() => {
     connectHandler();
-    retrieve();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
