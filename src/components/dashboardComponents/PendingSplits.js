314fa2335ec4ab1dd86c556aabd9e4e2a60907d9
Karan Patel <karan2605@hotmail.co.uk>
2023-01-03 14:18:13 +0000

    calls contract functions, updates profile on IPFS

--- src/components/dashboardComponents/PendingSplits.js
+++ src/components/dashboardComponents/PendingSplits.js
@@ -1,13 +1,23 @@
-import { Card, Table, Badge, Button } from "flowbite-react";
+import { Card, Table, Badge, Button, Alert } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 import { ActiveSplits } from "../Utilities";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-const PendingSplits = ({ globalData }) => {
+import { Contribute, TransferTotal, GetAccountData, UploadToIPFS } from "../Utilities";
+
+const PendingSplits = () => {
   const [splits, setSplits] = useState(null);
   const [account, setAccount] = useState(null);
+  const [showAlertContribute, setShowAlertContribute] = useState(false);
+  const [showAlertTransfer, setShowAlertTransfer] = useState(false);
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    const data = await GetAccountData();
+    setData(data);
+  };
 
   const colours = [
     "info",
@@ -38,9 +48,30 @@ const PendingSplits = ({ globalData }) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  const contribute = async (name, contribution) => {
+    await Contribute(name);
+
+    data.contributed = parseInt(data.contributed) + parseInt(contribution);
+    const newdata = new Blob([JSON.stringify(data)], { type: "application/json" });
+    await UploadToIPFS(data.account, newdata);
+
+    setShowAlertContribute(true);
+  };
+
+  const transferTotal = async (name, total) => {
+    await TransferTotal(name);
+
+    data.received = parseInt(data.received) + parseInt(total);
+    const newdata = new Blob([JSON.stringify(data)], { type: "application/json" });
+    await UploadToIPFS(data.account, newdata);
+
+    setShowAlertTransfer(true);
+  };
+
   useEffect(() => {
     findActive();
     getAccount();
+    getData();
   }, []);
 
   return (
@@ -49,6 +80,30 @@ const PendingSplits = ({ globalData }) => {
         "dark:border-4 dark:border-purple-600 dark:bg-stone-900 place-content-center"
       }
     >
+      {showAlertContribute && (
+        <Alert
+          color="success"
+          onDismiss={function onDismiss() {
+            setShowAlertContribute(false);
+          }}
+        >
+          <span>
+            <span className="font-medium">Contribution Added!</span>
+          </span>
+        </Alert>
+      )}
+      {showAlertTransfer && (
+        <Alert
+          color="success"
+          onDismiss={function onDismiss() {
+            setShowAlertTransfer(false);
+          }}
+        >
+          <span>
+            <span className="font-medium">Funds Collected!</span>
+          </span>
+        </Alert>
+      )}
       <div className="flex flex-col">
         <div className="mb-4 flex justify-between">
           <h1 className="text-4xl font-bold leading-none text-gray-900 dark:text-white">
@@ -72,7 +127,10 @@ const PendingSplits = ({ globalData }) => {
             {splits &&
               splits.map((item, key) => {
                 return (
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={key}>
+                  <Table.Row
+                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                    key={key}
+                  >
                     <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                       {JSON.parse(item).group}
                     </Table.Cell>
@@ -80,7 +138,11 @@ const PendingSplits = ({ globalData }) => {
                     <Table.Cell className="flex gap-2">
                       {JSON.parse(item).members.map((i, k) => {
                         return (
-                          <Badge color={colours[getRandomInt(0, 6)]} size="lg" key={k}>
+                          <Badge
+                            color={colours[getRandomInt(0, 6)]}
+                            size="lg"
+                            key={k}
+                          >
                             {i.slice(0, 5) + "..." + i.slice(38, 42)}
                           </Badge>
                         );
@@ -92,6 +154,7 @@ const PendingSplits = ({ globalData }) => {
                           gradientDuoTone="purpleToPink"
                           size="sm"
                           className="font-semibold "
+                          onClick={() => transferTotal(JSON.parse(item).group, JSON.parse(item).total)}
                         >
                           <span className="text-lg">Collect Funds</span>
                         </Button>
@@ -99,7 +162,8 @@ const PendingSplits = ({ globalData }) => {
                         <Button
                           gradientDuoTone="purpleToPink"
                           size="sm"
-                          className="font-semibold "
+                          className="font-semibold"
+                          onClick={() => contribute(JSON.parse(item).group)}
                         >
                           <span className="text-lg">Contribute</span>
                         </Button>
