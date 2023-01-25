40be503a9ac1a88594062e0922c341aaa08e0f03
Karan Patel <karan2605@hotmail.co.uk>
2023-01-25 18:37:25 +0000

    shows deposit events

--- src/components/SplitHistory.js
+++ src/components/SplitHistory.js
@@ -2,11 +2,41 @@ import DashboardBar from "./DashboardBar";
 import DashboardSidebar from "./DashboardSidebar";
 import SiteFooter from "./SiteFooter";
 
-import { Card } from "flowbite-react/lib/cjs/components/Card";
-import { Badge } from "flowbite-react/lib/cjs/components/Badge";
-import { Table } from "flowbite-react/lib/cjs/components/Table";
+import { useEffect, useState } from "react";
+
+import { GetEvents } from "./Utilities";
+
+import { Card, Badge, Table } from "flowbite-react";
 
 const SplitHistory = () => {
+  const [events, getEvents] = useState([]);
+
+  const getSplitEvents = async () => {
+    const events = await GetEvents();
+    console.log(events)
+    getEvents(events);
+  };
+
+  const colours = [
+    "info",
+    "failure",
+    "success",
+    "warning",
+    "indigo",
+    "purple",
+    "pink",
+  ];
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  useEffect(() => {
+    getSplitEvents();
+  }, []);
+
   return (
     <div className="flex flex-col">
       <DashboardBar />
@@ -24,140 +54,49 @@ const SplitHistory = () => {
               </h1>
             </div>
             <Table striped={true} className="text-md">
-                    <Table.Head className={"text-lg"}>
-                        <Table.HeadCell>
-                        Depositor
-                        </Table.HeadCell>
-                        <Table.HeadCell>
-                        Group
-                        </Table.HeadCell>
-                        <Table.HeadCell>
-                        Amount(ETH)
-                        </Table.HeadCell>
-                        <Table.HeadCell>
-                        <span className="sr-only">
-                            View TxNs
-                        </span>
-                        </Table.HeadCell>
-                    </Table.Head>
-                    <Table.Body className="divide-y">
-                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Head className={"text-lg"}>
+                <Table.HeadCell>Depositor</Table.HeadCell>
+                <Table.HeadCell>Group</Table.HeadCell>
+                <Table.HeadCell>Amount(ERC20)</Table.HeadCell>
+                <Table.HeadCell>Data</Table.HeadCell>
+                <Table.HeadCell>
+                  <span className="sr-only">View TxNs</span>
+                </Table.HeadCell>
+              </Table.Head>
+              <Table.Body className="divide-y">
+                {events &&
+                  events.map((item, key) => {
+                    return (
+                      <Table.Row
+                        className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                        key={key}
+                      >
                         <Table.Cell>
-                            <Badge color="purple" size="lg">
-                                0x213ef...j2nk
-                            </Badge>
+                          <Badge color={colours[getRandomInt(0, 6)]} size="lg">
+                            {item.args.sender.slice(0, 5) +
+                              "..." +
+                              item.args.sender.slice(38, 42)}
+                          </Badge>
                         </Table.Cell>
                         <Table.Cell>
-                            <Badge color="warning" size="lg">
-                                Group 1
-                            </Badge>
+                          <Badge color={colours[getRandomInt(0, 6)]} size="lg">
+                            {item.args.group}
+                          </Badge>
                         </Table.Cell>
+                        <Table.Cell>{item.args.amount.toString()}</Table.Cell>
                         <Table.Cell>
-                            0.005
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
+                          <a
                             href="/tables"
                             className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                            View TxNs
-                            </a>
+                          >
+                            View Full TxN Data
+                          </a>
                         </Table.Cell>
-                        </Table.Row>
-                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell>
-                            <Badge color="purple" size="lg">
-                                0x213ef...j2nk
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            <Badge color="failure" size="lg">
-                                Group 2
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            0.007
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
-                            href="/tables"
-                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                            View TxNs
-                            </a>
-                        </Table.Cell>
-                        </Table.Row>
-                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell>
-                            <Badge color="purple" size="lg">
-                                0x213ef...j2nk
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            <Badge color="success" size="lg">
-                                Group 3
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            0.002
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
-                            href="/tables"
-                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                            View TxNs
-                            </a>
-                        </Table.Cell>
-                        </Table.Row>
-                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell>
-                            <Badge color="purple" size="lg">
-                                0x213ef...j2nk
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            <Badge size="lg">
-                                Group 4
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            0.015
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
-                            href="/tables"
-                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                            View TxNs
-                            </a>
-                        </Table.Cell>
-                        </Table.Row>
-                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                        <Table.Cell>
-                            <Badge color="purple" size="lg">
-                                0x213ef...j2nk
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            <Badge color="pink" size="lg">
-                                Group 5
-                            </Badge>
-                        </Table.Cell>
-                        <Table.Cell>
-                            0.025
-                        </Table.Cell>
-                        <Table.Cell>
-                            <a
-                            href="/tables"
-                            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                            View TxNs
-                            </a>
-                        </Table.Cell>
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
+                      </Table.Row>
+                    );
+                  })}
+              </Table.Body>
+            </Table>
           </Card>
         </div>
       </div>
